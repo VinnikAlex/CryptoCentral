b96e2643b745e31f8b61bb31b5f3879edb8067c2
Alexander Vinnik <82071984+Sincroniiza@users.noreply.github.com>
2022-12-08 12:38:57 +1100

    Installed various libraries. Refactored and made code more modular/easier to understand

---
+++ cryptohold/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+// /** @format */
+
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectedOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
---
+++ cryptohold/src/Components/index.js
@@ -0,0 +1,6 @@
+/** @format */
+
+// all components go here
+
+export { default as Navbar } from "./Navbar";
+export { default as Homepage } from "./Home";
